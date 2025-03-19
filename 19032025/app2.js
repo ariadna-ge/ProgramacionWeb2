2cf37127a2d6637ccc9913f3c4beede32267601a
ariadna-ge <aridenn1503@hotmail.com>
2025-03-19 15:55:37 -0600

    Métodos de modificación de un Array

---
+++ 19032025/app2.js
@@ -0,0 +1,28 @@
+//Ejemplo de push
+/* const frutas = ["Plátano"];
+frutas.push("Sandía");
+console.log(frutas); */
+
+//Ejemplo de unshift
+/* const frutas = ["Plátano"];
+frutas.unshift("Sandía");
+console.log(frutas); */
+
+//Ejemplo de push y pop
+/* const frutas = ["Plátano","Pera"];
+frutas.push("Sandía");
+console.log(frutas);
+const frutaEliminada= frutas.pop();
+console.log(frutaEliminada);
+console.log(frutas);
+ */
+
+//Ejemplo de push y shift
+const frutas = ["Plátano","Pera"];
+    frutas.push("Sandía");
+    console.log(frutas);
+    
+const frutaEliminada= frutas.shift();
+    console.log(frutaEliminada);
+
+    console.log(frutas);
