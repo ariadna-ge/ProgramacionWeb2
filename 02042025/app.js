ef13b2a240b1ae636586ef6dec52ee9cb73437fb
ariadna-ge <aridenn1503@hotmail.com>
2025-04-06 18:14:01 -0600

    Explicacion inicial de js y php

---
+++ 02042025/app.js
@@ -0,0 +1,7 @@
+/* console.log(document);
+console.log(document.title);
+console.log(document.body); */
+
+console.log(document.getElementById("tituloWeb"));
+console.log(document.getElementById("tituloWeb").textContent);
+
