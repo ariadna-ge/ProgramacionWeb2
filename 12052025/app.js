491733f93dc5f257616501dfadb958e465378f16
ariadna-ge <aridenn1503@hotmail.com>
2025-05-23 20:03:16 -0600

    Parte II. Conexión

--- 12052025/app.js
+++ 12052025/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const { use } = require('react');
+//const { use } = require('react');
 const app = express();
 
 //manejo de peticion por medio de req
 app.use(bodyParser.urlencoded({extended:false}));
 
 ///Configuración de plantilla
-app.set/('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 //conexión de la base de datos
 const db = mysql.createConnection({
@@ -31,7 +31,7 @@ db.connect(err =>{
 //inicio del server
 const port = 3008;
 app.listen(port, ()=>{
-    console.log(`http://127.0.0.1${port}`);
+    console.log(`http://127.0.0.1:${port}`);
 });
 
 //mostrar lista de usuarios
@@ -44,7 +44,7 @@ app.get('/',(req,res)=>{
             //no se encontro el usuario, o hay un error para el desarrollador
             console.log('Error al recuperar el ususario', err); 
             //mostrar la información al usuario
-            res.send('Error, no se recuperan los datos de la db')
+            res.send('Error, no se recuperan los datos de la db');
         }else{
             res.render('index', {users: results});
         }
@@ -54,12 +54,8 @@ app.get('/',(req,res)=>{
 //modulo para agregar un usuario
 app.post('/add', (req,res)=>{
     const {name, email} = req.body;
-    /*
-    Nombre: Ariadna
-    Correo: ariadnagarcia175@aragon.unam.mx
-    */
-    const query = 'INSERT INTO users (name, email) VALUES (?,?)';
-    db.query(insertarRegistro, [name, email], (err) =>{
+    const insertarRegistro = 'INSERT INTO users (name, email) VALUES (?,?)';
+    db.query(insertarRegistro, [name, email], (err) => {
         if (err) {
             console.log('Error al agregar usuario', err);
             res.send('Error');
@@ -68,3 +64,49 @@ app.post('/add', (req,res)=>{
         }
     });
 });
+
+//modulo para editar usuario
+app.get('/edit/:id',(req,res)=>{
+    const{id}= req.params;
+    const buscarUsuarioID = 'SELECT * FROM users WHERE id = ?';
+    db.query(buscarUsuarioID,[id],(err,results)=>{
+        if(err){
+            console.log("Error en la DB", err);
+        }else{
+            res.render('edit',{user:results[0]});
+        }
+    });
+
+});
+
+//actualizar
+app.post('/update/:id',(req,res)=>{
+    const {id} = req.params;
+    const {name,email}=req.body;
+
+    const query = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+    db.query(query,[name,email,id],(err)=>{
+        if(err){
+            console.error("Error", err);
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+//eliminar
+app.get('/delete/:id',(req,res)=>{
+    const {id} = req.params;
+    const eliminarUsuarioId = 'DELETE FROM users WHERE id= ?';
+
+    db.query(eliminarUsuarioId,[id], (err)=>{
+        if (err) {
+            console.error('Errror al eliminar en la bd');
+            res.send('Error en la BD');
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+// Estructura de rutas 1. DB, 2. Lista de negocio, 3. Public con CSS, IMG, View, APP
